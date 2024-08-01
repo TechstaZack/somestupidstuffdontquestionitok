6fcadf1bc87e56320c45c9210c50b3e450b69aba
TechstaZack <127003749+TechstaZack@users.noreply.github.com>
2024-08-02 09:34:27 +1000

    Update index.js

--- index.js
+++ index.js
@@ -37,13 +37,13 @@ function swaptovisit(){
                     <h5 class="errorWhenLoggingIn" id="Lnoexist"></h5>
                                         </form>
                       <span>
-                  <button class="btn btn-success paddinpls" id="id1"><svg xmlns="http://www.w3.org/2000/svg" class='svgbonus' width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                  <button class="btn btn-success paddinpls" id="id1" onclick='normalgo()'><svg xmlns="http://www.w3.org/2000/svg" class='svgbonus' width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
                     <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
                   </svg> Visit</button>
                           <h1> </h1>
 
-                          <button class="btn btn-danger paddinpls" id="skip"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
+                          <button class="btn btn-danger paddinpls" id="skip" onclick='skipplease()'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
   <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
 </svg> Visit (skip link info)</button>
@@ -94,10 +94,10 @@ function swaptocreate(){
                                 <option value="techsta.link">Techsta.Link</option>
                               </select>
                       </div>
-                    <button class="btn btn-primary" type='submit' id="createURL"><svg xmlns="http://www.w3.org/2000/svg" class="svgbonus" width="16" height="16" fill="currentColor" class="bi bi-hammer" viewBox="0 0 16 16">
+                                        </form>
+                    <button class="btn btn-primary" type='submit' id="createURL" onclick='createtheURL()'><svg xmlns="http://www.w3.org/2000/svg" class="svgbonus" width="16" height="16" fill="currentColor" class="bi bi-hammer" viewBox="0 0 16 16">
   <path d="M9.972 2.508a.5.5 0 0 0-.16-.556l-.178-.129a5.009 5.009 0 0 0-2.076-.783C6.215.862 4.504 1.229 2.84 3.133H1.786a.5.5 0 0 0-.354.147L.146 4.567a.5.5 0 0 0 0 .706l2.571 2.579a.5.5 0 0 0 .708 0l1.286-1.29a.5.5 0 0 0 .146-.353V5.57l8.387 8.873A.5.5 0 0 0 14 14.5l1.5-1.5a.5.5 0 0 0 .017-.689l-9.129-8.63c.747-.456 1.772-.839 3.112-.839a.5.5 0 0 0 .472-.334z"/>
 </svg> Create</button>
-                  </form>
                 <hr>
                 <button class='btn btn-info' id='swaptheweb' onclick='swaptovisit()'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-right" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5m14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5"/>
@@ -119,12 +119,9 @@ function copyToClipboard(){
 function restart(){
     window.location.reload()
 }
-const hehabooya = document.getElementById('id1')
-const skip = document.getElementById('skip')
-const nosubmit = document.getElementById('linkURL')
 
 //skip
-skip.addEventListener('click', function(){
+function skipplease(){
     console.log('ran')
   let code = document.getElementById('wegohereboys').value;
   let domain = document.getElementById('domaintogo').value;
@@ -150,7 +147,7 @@ code = code.replace('+','')
     }
   })
 })
-hehabooya.addEventListener('click', function(){
+function normalgo(){
         console.log('ran')
   let code = document.getElementById('wegohereboys').value;
   let domain = document.getElementById('domaintogo').value;
@@ -191,9 +188,8 @@ code = code.replace('+',"")
  // window.location.href = urlto
 //})
 
-const form = document.getElementById('createURL')
-form.addEventListener('submit', function(e){
-    e.preventDefault();
+
+function createtheURL(){
     const database = firebase.database()
     let shortener = document.getElementById('short').value;
     shortener = shortener.toLowerCase()
